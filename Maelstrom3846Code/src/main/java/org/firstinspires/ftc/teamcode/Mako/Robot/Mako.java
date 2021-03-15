e61ca461d995e45b9dcaaaea6aac635f18878efc
realamoghm <43323507+realamoghm@users.noreply.github.com>
2021-03-15 17:42:55 -0400

    v1.0 - 03/15/2021
    - Fixed NullPointerException (simply forgot to initialize encoders)
    Don't merge into main until code is tested on real robot

--- Maelstrom3846Code/src/main/java/org/firstinspires/ftc/teamcode/Mako/Robot/Mako.java
+++ Maelstrom3846Code/src/main/java/org/firstinspires/ftc/teamcode/Mako/Robot/Mako.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import Library17822.MidnightControlSystems.MidnightPID.MidnightPIDController;
 import Library17822.MidnightDriveTrains.MidnightMechanumDriveTrain;
+import Library17822.MidnightMotors.MidnightMotor;
 import Library17822.MidnightMotors.MidnightMotorModel;
 import Library17822.MidnightPositionTracker;
 import Library17822.MidnightRobot;
@@ -16,16 +17,23 @@ import static Library17822.MinightResources.MidnightUtils.setTracker;
 import static Library17822.MinightResources.MidnightUtils.turnController;
 
 public class Mako extends MidnightRobot {
+    public MidnightMotor encoder1, encoder2;
 
     @Override
     public void mapHardware (HardwareMap hardwareDevices) {
         driveTrain = new MidnightMechanumDriveTrain(hardwareDevices, MidnightMotorModel.NEVEREST40);
+
+
         dash = MidnightDashBoard.getDash();
     }
 
     @Override
     public void init(HardwareMap hardwareMap) {
         mapHardware(hardwareMap);
+
+        encoder1 = new MidnightMotor("xEncoder", MidnightMotorModel.REVTHROUGHBORE,hardwareMap);
+        encoder2 = new MidnightMotor("yEncoder", MidnightMotorModel.REVTHROUGHBORE, hardwareMap);
+        tracker = new MidnightPositionTracker(encoder1,encoder2,hardwareMap);
 
         tracker.setPosition(MidnightPositionTracker.DeadWheelPosition.BOTH_CENTER);
         tracker.setXRadius(9.00);
