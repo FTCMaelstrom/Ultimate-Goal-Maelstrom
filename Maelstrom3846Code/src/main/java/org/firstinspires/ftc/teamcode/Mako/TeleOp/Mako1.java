5f837c867e821f798d24578b503ab3460b963fa5
realamoghm <43323507+realamoghm@users.noreply.github.com>
2021-04-05 16:06:35 -0400

    v2.0.5 - 04/05/2021
    Added some new things.
    Changes Include:
    - New endgameModeEnabled boolean which allows selection to endgameMode in Endgame by pressing A on the gamepad and selection out of endgameMode by pressing B on the gamepad.
    - Added new variable speed method for rotator arm and changed controls for rotator arm and claw to endgameMode.

--- Maelstrom3846Code/src/main/java/org/firstinspires/ftc/teamcode/Mako/TeleOp/Mako1.java
+++ Maelstrom3846Code/src/main/java/org/firstinspires/ftc/teamcode/Mako/TeleOp/Mako1.java
@@ -9,17 +9,19 @@ import org.firstinspires.ftc.teamcode.Mako.Robot.Mako;
 import MidnightLibrary.MidnightResources.MidnightLinearOpMode;
 import MidnightLibrary.MidnightRobot;
 
+import static org.firstinspires.ftc.teamcode.Mako.Subsystems.Constants.ROTATOR_POWER;
+
 /**
  * Created by Amogh Mehta
  * Project: FtcRobotController_Ultimate-Goal_prod2
- * Last Modified: 4/5/21 12:24 PM
- * Last Updated: 4/5/21 12:31 PM
+ * Last Modified: 4/5/21 3:57 PM
+ * Last Updated: 4/5/21 3:57 PM
  **/
 
 @TeleOp(name = "Mako1", group = "Mako")
 public class Mako1 extends MidnightLinearOpMode {
     private Mako mako = new Mako();
-    double rotatorSpeed = 1;
+    boolean endgameModeEnabled = false;
 
     @Override
     public void runLinearOpMode() {
@@ -49,62 +51,56 @@ public class Mako1 extends MidnightLinearOpMode {
             packet.put("RightBackVelocity: ", mako.driveTrain.getVelocityRightBack());
             dashboard.sendTelemetryPacket(packet);
 
-            /*
-            dash.create("LFPow: ", mako.driveTrain.getPowerLeftFront());
-            dash.create("LBPow: ", mako.driveTrain.getPowerLeftBack());
-            dash.create("RFPow: ", mako.driveTrain.getPowerRightFront());
-            dash.create("RBPow: ", mako.driveTrain.getPowerRightBack());
-            */
-
-            /*
-            dash.create("LFVel: ", mako.driveTrain.getVelocityLeftFront());
-            dash.create("LBVel: ", mako.driveTrain.getVelocityLeftBack());
-            dash.create("RFVel: ", mako.driveTrain.getVelocityRightFront());
-            dash.create("RBVel: ", mako.driveTrain.getVelocityRightBack());
-            */
-
-
             mako.rotator.setStopMotor();
             mako.rotator.resetEncoder();
             mako.rotator.runWithoutEncoderNoZeroPowerSet();
             mako.rotator.brakeMode();
             mako.rotator.setClosedLoop(false);
 
-            /* Below is a prototype rotator control */
-            if (gamepad1.right_trigger > 0.05) {
-                mako.rotator.setPower(gamepad1.right_trigger);
-            }
-            if (gamepad1.left_trigger > 0.05) {
-                mako.rotator.setPower(-1*gamepad1.left_trigger);
-            }
-
-            /* This is the button based rotator control */
+            /*
+            // This is the button based rotator control
             if (gamepad1.right_bumper && !gamepad1.left_bumper) {
-                mako.rotator.setPower(rotatorSpeed);
+                mako.rotator.setPower(ROTATOR_POWER);
             }
             if (!gamepad1.left_bumper && !gamepad1.right_bumper) {
                 mako.rotator.setPower(0);
             }
             if (gamepad1.left_bumper && !gamepad1.right_bumper) {
-                mako.rotator.setPower(-rotatorSpeed);
+                mako.rotator.setPower(-ROTATOR_POWER);
             }
+            */
 
-            /* This is the claw servo control */
+            // This is the claw servo control
             mako.claw.driverControl(gamepad1);
-            if (gamepad1.a) {
-                mako.claw.open();
-            }
 
-            packet.put("Rotator Speed: ", rotatorSpeed);
+            packet.put("Rotator Speed: ", ROTATOR_POWER);
             packet.put("Rotator Motor Power: ", mako.rotator.getPower());
             packet.put("Rotator Velocity: ", mako.rotator.getVelocity());
+            dashboard.sendTelemetryPacket(packet);
 
-            /*
-            dash.create("Rotator Speed: ", rotatorSpeed);
-            dash.create("Rotator Motor: ", mako.rotator.getPower());
-            dash.create("Rotator Velocity: ", mako.rotator.getVelocity());
-            dash.update();
-            */
+            if (gamepad1.a) {
+                endgameModeEnabled = true;
+            } else if (gamepad1.b) {
+                endgameModeEnabled = false;
+            }
+
+            dash.create("endgameModeEnabled: ", endgameModeEnabled);
+
+            while (endgameModeEnabled) {
+                mako.rotator.setPower(ROTATOR_POWER * gamepad1.left_trigger - gamepad1.right_trigger);
+                if (gamepad1.dpad_down) {
+                    ROTATOR_POWER -= 0.01;
+                } else if (gamepad1.dpad_up) {
+                    ROTATOR_POWER = +0.01;
+                }
+
+                if (gamepad1.right_bumper) {
+                    mako.claw.close();
+                } else if (gamepad1.left_bumper) {
+                    mako.claw.open();
+                }
+            }
+
         }
     }
 }
